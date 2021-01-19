68dcd67ecca945295bb1f5c7b1c6ff69c5ae4b8b
Roman <fioletmail@gmail.com>
2021-01-19 20:53:52 +0300

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/*Vue.component('example-component', require('./components/ExampleComponent.vue').default);*/
+/*Vue.component('contacts', require('./components/Contacts.vue'));*/
+
+import Contacts from './components/Contacts.vue';
+Vue.component('Contacts', Contacts);
+
+const app = new Vue({
+    el: '#app'
+});
